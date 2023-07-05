b5cade287d603d2dc005d66420c5fa4ac62a1ad2
김기준 <kkjsample@gmail.com>
2023-07-05 16:48:59 +0900

    aaaaa

--- list.js
+++ list.js
@@ -29,4 +29,60 @@ async function fetch(pageno=1, pagesize=10) {
   }
 }
 
+function printContacts(contacts) {
+  const $parent = $('#tbody');
+  for(c of contacts) {
+    const html = `
+      <tr>
+        <td>${c.no}</td>
+        <td><a href='read.html?no=${c.no}'>${c.name}</a></td>
+        <td>${c.tel}</td>
+        <td>${c.address}</td>
+      </tr>
+    `;
+    $parent.append(html);
+  }
+}
+
+function getPagination({pageno, totalcount, pagesize, blocksize=5}) {
+  // totalcount      페이지 개수
+  //  101~110           11
+  const 페이지개수 = Math.ceil(totalcount/pagesize);
+
+  /*       prev  start      end     next
+    1~5:    0     1   2 3 4  5       6
+    6~10:   5     6   7 8 9  10      11
+    11 :    10    11
+  */
+  const prev = Math.floor((pageno-1)/blocksize)*blocksize;
+  const start = prev + 1;
+  let end = prev + blocksize;
+  let next = end + 1;
+  if(end>=페이지개수) {
+    end = 페이지개수;
+    next = 0;
+  }
+  return {prev, start, end, next, pageno};
+}
+
+function printPagination({prev, start, end, next, pageno}) {
+  const $parent = $('#pagination');
+  if(prev>0) {
+    const html=`<li class="page-item"><a href="list.html?pageno=${prev}" class="page-link">이전으로</a></li> `;
+    $parent.append(html);
+  }
+  for(let i=start; i<=end; i++) {
+    let classname = 'page-item';
+    if(i===pageno)
+      classname = 'page-item active'
+    const html=`<li class="${classname}"><a href="list.html?pageno=${i}" class="page-link">${i}</a></li> `;
+    $parent.append(html);
+  }
+  if(next>0) {
+    const html=`<li class="page-item"><a href="list.html?pageno=${next}" class="page-link">다음으로</a></li> `;
+    $parent.append(html);
+  }
+}
+
+
 
