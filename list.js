416cf0b4032d2384fc595e25b0b81e79825fb237
김기준 <kkjsample@gmail.com>
2023-07-05 14:42:04 +0900

    https http ajax설정

--- list.js
+++ list.js
@@ -16,10 +16,8 @@ function getPageno() {
 
 // 기본 매개변수(default parameter)
 async function fetch(pageno=1, pagesize=10) {
-  console.log(pagesize);
   const api = 'http://sample.bmaster.kro.kr/contacts';
   const url = `${api}?pageno=${pageno}&pagesize=${pagesize}`;
-  console.log(url);
   // $.ajax()는 병렬 처리(비동기 처리)되는 코드 -> 언제 끝날 지 모른다
   // 비동기코드를 리턴받는 result는 "미래에 값이 들어올 것이다"란 값을 가진다
   // (Promise)
